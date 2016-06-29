f7742544347990e78a1793c0091f76e0d7f86c4a
baoxue <Blizzard_xc@126.com>
2016-06-29 14:05:41 +0800

    review

--- Assets/Script/Moudle/View/UI/Core/UIManager.cs
+++ Assets/Script/Moudle/View/UI/Core/UIManager.cs
@@ -7,26 +7,31 @@ using Common.Tool;
 public class UIManager : Singleton<UIManager>
 {
     private Dictionary<Type, UIBase> m_CurrentWindowStore;
+    private Camera m_UICamera;
+    private UIRoot m_UIRoot;
 
     public UIManager()
     {
         m_CurrentWindowStore = new Dictionary<Type, UIBase>();
     }
-    public void OpenWindow(Type t, object param)
+    public UIBase OpenWindow(Type t, object param)
     {
         UIBase ui = null;
         m_CurrentWindowStore.TryGetValue(t, out ui);
         if (null == ui)
         {
             ui = Activator.CreateInstance(t) as UIBase;
-            ui.DoCreate();
+            m_CurrentWindowStore.Add(t, ui);
+
+            ui.DoCreate(OnWindowLoaded);
         }
         if (ui.IsOpen())
         {
             // do nothing
-            return;
+            return ui;
         }
         ui.DoOpen(param);
+        return ui;
     }
     public void OpenWindow<T>(object param) where T : UIBase
     {
@@ -46,6 +51,7 @@ public class UIManager : Singleton<UIManager>
             return;
         }
         ui.DoClose();
+        m_CurrentWindowStore.Remove(t);
     }
     public void HideWindow<T>() where T : UIBase
     {
@@ -61,5 +67,48 @@ public class UIManager : Singleton<UIManager>
             return;
         }
         ui.DoHide();
+    }
+    public Camera GetUICamera()
+    {
+        return m_UICamera;
+    }
+
+    public UIRoot GetUIRoot()
+    {
+        return m_UIRoot;
+    }
+    private void OnWindowLoaded(GameObject windowRoot, UIBase windowBase)
+    {
+
+    }
+    //private int GetCurrentWindowDeepth(WindowLayer layer)
+    //{
+    //    int currentLayerDeepth = ++m_LayerIndexStore[layer].m_iCurrent;
+    //    if (currentLayerDeepth > m_LayerIndexStore[layer].m_iMax)
+    //    {
+    //        currentLayerDeepth = ResetDeepth(layer);
+    //    }
+    //    if (currentLayerDeepth > m_LayerIndexStore[layer].m_iMax)
+    //    {
+    //        Debuger.LogError("panel deepth out of range");
+    //    }
+    //    return currentLayerDeepth;
+    //}
+    //private int ResetDeepth(WindowLayer layer)
+    //{
+    //    int lastWindowDeepth = m_LayerIndexStore[layer].m_iMin;
+    //    for (int i = 0; i < m_ActivedWindowQueue[layer].Count; ++i)
+    //    {
+    //        m_ActivedWindowQueue[layer][i].ResetDeepth(lastWindowDeepth + 1);
+    //        lastWindowDeepth = m_ActivedWindowQueue[layer][i].GetMaxDeepthValue();
+    //    }
+    //    m_LayerIndexStore[layer].m_iCurrent = lastWindowDeepth + 1;
+    //    return m_LayerIndexStore[layer].m_iCurrent;
+    //}
+    public void HideAllWindow()
+    {
+    }
+    public void CloseAllWindow()
+    {
     }
 }
