b722dee40d199d3363db7921237af43a6d2bd9d6
baoxue <Blizzard_xc@126.com>
2016-08-17 19:45:26 +0800

    update

---
+++ Assets/Editor/ResourceReleaseTool/ResourceReleaseTool.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceReleaseTool : MonoBehaviour
+{
+    public enum UIMode
+    {
+        NGUI,
+        UGUI,
+    }
+    private string m_strReleaseResRootPath = "";
+    private UIMode m_UIMode;
+    private string m_strForceSinglePath = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
