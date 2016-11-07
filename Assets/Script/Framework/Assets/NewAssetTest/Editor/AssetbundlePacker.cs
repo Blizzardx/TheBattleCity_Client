d51ff74a4d4e8a8db79eb3f782257150ff88cb35
baoxue <Blizzard_xc@126.com>
2016-11-07 18:02:12 +0800

    add code gen tool

--- Assets/Script/Framework/Assets/NewAssetTest/Editor/AssetbundlePacker.cs
+++ Assets/Script/Framework/Assets/NewAssetTest/Editor/AssetbundlePacker.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
 
 namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
 {
     class AssetbundlePacker
     {
+        private string m_strUGUIAtlasPath;
+        private string m_strNGUIAtlasPath;
+        private string m_strDependentAssetRootPath;
+        private string m_strIndependentAssetPath;
+        private string m_strOutputPath;
 
+        void test()
+        {
+            
+        }
     }
 }
