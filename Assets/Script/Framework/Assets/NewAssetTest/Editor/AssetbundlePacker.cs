c2256ef86cbb72fa9b172b853075041cfc5479c8
baoxue <Blizzard_xc@126.com>
2016-11-10 19:57:11 +0800

    update editor

--- Assets/Script/Framework/Assets/NewAssetTest/Editor/AssetbundlePacker.cs
+++ Assets/Script/Framework/Assets/NewAssetTest/Editor/AssetbundlePacker.cs
@@ -8,35 +8,20 @@ namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
 {
     class AssetbundlePacker
     {
-        private class PackerInfo
-        {
-            public PackerInfo()
-            {
-                
-            }
-            public PackerInfo(string path, string bundleName)
-            {
-                this.path = path;
-                this.bundleName = bundleName;
-            }
-            public string path;
-            public string bundleName;
-        }
         private string[] m_IgnoreList = new string[] {".meta",".svn",".cs"};
         private string m_strUGUIAtlasPath;
         private string m_strNGUIAtlasPath;
         private string m_strDependentAssetRootPath;
-        private string m_strIndependentAssetPath;
         private string m_strOutputPath;
 
         private TextureImporterType     m_TextureType;
         private TextureImporterFormat   m_TextureFormate;
 
-        public void BuildAssetBundle
+        #region public interface
+        public void BeginSetBundleName
             (   string UGUIAtlasPath,
                 string NGUIAtlasPath,
                 string DependentAssetRootPath,
-                string IndependentAssetPath,
                 string OutputPath, 
                 TextureImporterType   TextureType,
                 TextureImporterFormat TextureFormate)
@@ -44,55 +29,80 @@ namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
             m_strUGUIAtlasPath = UGUIAtlasPath;
             m_strNGUIAtlasPath = NGUIAtlasPath;
             m_strDependentAssetRootPath = DependentAssetRootPath;
-            m_strIndependentAssetPath = IndependentAssetPath;
             m_strOutputPath = OutputPath;
 
             m_TextureFormate = TextureFormate;
             m_TextureType = TextureType;
             SetBundleName();
         }
+        public void BeginBuild()
+        {
+            Exception e = null;
+            BeginBuildAssetBundle(ref e);
+            CheckAndPrintException(e);
+        }
+        public void ResetAllBundleName(string AppDataPath)
+        {
+            if (Directory.Exists(AppDataPath))
+            {
+                var dir = new DirectoryInfo(AppDataPath);
+                var files = dir.GetFiles("*", SearchOption.AllDirectories);
+                for (var i = 0; i < files.Length; ++i)
+                {
+                    var fileInfo = files[i];
+                    // is in ignore list
+                    if (!IsInIgnoreList(fileInfo.FullName))
+                    {
+                        AssetImporter tmp = AssetImporter.GetAtPath(GetNameByPath(fileInfo.FullName));
+                        if (tmp != null)
+                        {
+                            tmp.assetBundleName = null;
+                        }
+                    }
+                }
+            }
+        }
+        public void GenAssetToBundleFindMap()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region system function
         private void SetBundleName()
         {
             Exception e = null;
 
             // initialize
             CheckParamter(ref e);
-            if (!CheckException(e))
+            if (!CheckAndPrintException(e))
+            {
+                return;
+            }
+
+            // check bundle name
+            if (!CheckAssetNameIsVailed())
             {
                 return;
             }
 
             // set dependents bundle name
             SetDependentsBundleName(ref e);
-            if (!CheckException(e))
-            {
-                return;
-            }
-
-            // set independents bundle name
-            SetIndependentsBundleName(ref e);
-            if (!CheckException(e))
+            if (!CheckAndPrintException(e))
             {
                 return;
             }
             
             // set UGUI Atlas bundle name
             SetUGUIAtlasBundleName(ref e);
-            if (!CheckException(e))
+            if (!CheckAndPrintException(e))
             {
                 return;
             }
             
             // set NGUI Atlas bundle name
             SetNGUIAtlasBundleName(ref e);
-            if (!CheckException(e))
-            {
-                return;
-            }
-
-            // Build
-            BeginBuildAssetBundle(ref e);
-            if (!CheckException(e))
+            if (!CheckAndPrintException(e))
             {
                 return;
             }
@@ -104,11 +114,6 @@ namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
                 e = new Exception("can't fine dependent asset root path");
                 return;
             }
-            if (!Directory.Exists(m_strIndependentAssetPath))
-            {
-                e = new Exception("can't fine independent asset root path");
-                return;
-            }
             if (!Directory.Exists(m_strOutputPath))
             {
                 e = new Exception("can't fine output path");
@@ -117,7 +122,7 @@ namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
         }
         private void SetDependentsBundleName(ref Exception e)
         {
-            List<PackerInfo> list = new List<PackerInfo>();
+            List<string> list = new List<string>();
             var dir = new DirectoryInfo(m_strDependentAssetRootPath);
             var files = dir.GetFiles("*", SearchOption.AllDirectories);
             for (int i = 0; i < files.Length; ++i)
@@ -125,15 +130,15 @@ namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
                 if (!IsInIgnoreList(files[i].Name))
                 {
                     // add to list
-                    list.Add(new PackerInfo(files[i].FullName,string.Empty));
+                    list.Add(files[i].FullName);
                 }
             }
             Dictionary<string, int> refCountMap = new Dictionary<string, int>();
-
+            Dictionary<string, string> refFindMap = new Dictionary<string, string>();
             for (int i = 0; i < list.Count; ++i)
             {
                 var elem = list[i];
-                var depList = AssetDatabase.GetDependencies(elem.path);
+                var depList = AssetDatabase.GetDependencies(GetNameByPath(elem));
                 for (int j = 0; j < depList.Length; ++j)
                 {
                     if (refCountMap.ContainsKey(depList[j]))
@@ -144,19 +149,81 @@ namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
                     {
                         refCountMap.Add(depList[j],1);
                     }
+                    // mark 
+                    if (refFindMap.ContainsKey(depList[i]))
+                    {
+                        refFindMap.Add(depList[i], elem);
+                    }
+                    else
+                    {
+                        refFindMap[depList[i]] = elem;
+                    }
                 }
             }
-            
-        }
-        private void SetIndependentsBundleName(ref Exception e)
-        {
-            
+
+            foreach (var elem in refCountMap)
+            {
+                string bundleName = string.Empty;
+                if (elem.Value == 1 )
+                {
+                    // check is resource in build path
+                    if (IsPathInBuildPath(elem.Key))
+                    {
+                        bundleName = GetBundleNameByPath(elem.Key);
+                        DoSetBundleName(elem.Key, bundleName);
+                    }
+                    // check resource is scene file
+                    else if (IsPathSceneFile(refFindMap[elem.Key]))
+                    {
+                        DoSetBundleName(elem.Key, GetSceneDepBundleName(refFindMap[elem.Key]));
+                    }
+                    else
+                    {
+                        //
+                        bundleName = GetBundleNameByPath(refFindMap[elem.Key]);
+                        DoSetBundleName(elem.Key, bundleName);
+                    }
+                }
+                else
+                {
+                    bundleName = GetBundleNameByPath(elem.Key);
+                    DoSetBundleName(elem.Key, bundleName);
+                }
+            }
         }
         private void SetUGUIAtlasBundleName(ref Exception e)
         {
             if (!Directory.Exists(m_strUGUIAtlasPath))
             {
                 return;
+            }
+
+            List<string> list = new List<string>();
+            var dir = new DirectoryInfo(m_strUGUIAtlasPath);
+            var files = dir.GetFiles("*", SearchOption.AllDirectories);
+            for (int i = 0; i < files.Length; ++i)
+            {
+                if (!IsInIgnoreList(files[i].Name))
+                {
+                    // add to list
+                    list.Add(files[i].FullName);
+                }
+            }
+            for (int i = 0; i < list.Count; ++i)
+            {
+                var importer = TextureImporter.GetAtPath(list[i]) as TextureImporter;
+                if (importer != null)
+                {
+                    importer.spritePackingTag = list[i];
+                    importer.textureType = TextureImporterType.Sprite;
+                    importer.textureFormat = TextureImporterFormat.AutomaticTruecolor;
+                    importer.maxTextureSize = 1024;
+                    importer.mipmapEnabled = false;
+                    importer.filterMode = FilterMode.Trilinear;
+
+                    importer.assetBundleName = list[i];
+                    importer.SaveAndReimport();
+                }
             }
         }
         private void SetNGUIAtlasBundleName(ref Exception e)
@@ -165,11 +232,38 @@ namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
             {
                 return;
             }
+            List<string> list = new List<string>();
+            var dir = new DirectoryInfo(m_strUGUIAtlasPath);
+            var files = dir.GetFiles("*.prefab", SearchOption.AllDirectories);
+            for (int i = 0; i < files.Length; ++i)
+            {
+                if (!IsInIgnoreList(files[i].Name))
+                {
+                    // add to list
+                    list.Add(files[i].FullName);
+                }
+            }
+            for (int i = 0; i < list.Count; ++i)
+            {
+                var elem = list[i];
+                var depList = AssetDatabase.GetDependencies(GetNameByPath(elem));
+                for (int j = 0; j < depList.Length; ++j)
+                {
+                    if (!IsPathInNGUIPath(depList[j]))
+                    {
+                        string errorMsg = "NGUI Atlas is not in correct path "+ depList[i];
+                        Debug.LogError(errorMsg);
+                        e = new Exception(errorMsg);
+                        return;
+                    }
+                    DoSetBundleName(elem, elem + ".bundle");
+                }
+            }
         }
         private void BeginBuildAssetBundle(ref Exception e)
         {
         }
-        private bool CheckException(Exception e)
+        private bool CheckAndPrintException(Exception e)
         {
             if (e != null)
             {
@@ -178,21 +272,54 @@ namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
             }
             return true;
         }
-        private void DoSetBundleName(List<PackerInfo> list)
+        private void DoSetBundleName(string path,string bundleName)
         {
-            for (int i = 0; i < list.Count; ++i)
+            AssetImporter tmp = AssetImporter.GetAtPath(path);
+            if (tmp != null)
             {
-                PackerInfo info = list[i];
-                AssetImporter tmp = AssetImporter.GetAtPath(info.path);
-                if (tmp != null)
+                tmp.assetBundleName = bundleName;
+            }
+            else
+            {
+                Debug.LogWarning("path inexist " + path);
+            }
+        }
+        private string GetBundleNameByPath(string path)
+        {
+            int index = path.LastIndexOf('/');
+            return path.Substring(index + 1) + ".bundle";
+        }
+        private bool CheckAssetNameIsVailed()
+        {
+            string[] pathList = new string[] { m_strUGUIAtlasPath ,m_strNGUIAtlasPath,m_strDependentAssetRootPath};
+            for (int i = 0; i < pathList.Length; ++i)
+            {
+                pathList[i] = GetNameByPath(pathList[i]);
+            }
+
+            var depList = AssetDatabase.GetDependencies(pathList);
+            bool isOk = true;
+            HashSet<string> nameStore = new HashSet<string>();
+
+            for (int i = 0; i < depList.Length; ++i)
+            {
+                var name = depList[i];
+                if (!IsAssetNameVailed(name))
                 {
-                    tmp.assetBundleName = info.bundleName;
+                    Debug.LogError(" name is not vailed : " + name);
+                    isOk = false;
+                }
+                if (nameStore.Contains(name))
+                {
+                    Debug.LogError(" name is alreay exist : " + name);
+                    isOk = false;
                 }
                 else
                 {
-                    Debug.LogWarning("path inexist " + info.path);
+                    nameStore.Add(name);
                 }
             }
+            return isOk;
         }
         private bool IsAssetNameVailed(string tmpName)
         {
@@ -220,5 +347,33 @@ namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
             }
             return false;
         }
+        private bool IsPathInBuildPath(string path)
+        {
+            return path.StartsWith(m_strDependentAssetRootPath);
+        }
+        private bool IsPathInNGUIPath(string path)
+        {
+            return path.StartsWith(m_strNGUIAtlasPath);
+        }
+        private bool IsPathSceneFile(string path)
+        {
+            return path.EndsWith(".unity");
+        }
+        private string GetSceneDepBundleName(string path)
+        {
+            return path+"_scene";
+        }
+        private string GetNameByPath(string path)
+        {
+            var fullName = path.Replace('\\', '/');
+            var index = fullName.IndexOf("Assets/");
+            if (index != -1)
+            {
+                return fullName.Substring(index);
+            }
+            return fullName;
+        }
+        #endregion
+
     }
 }
---
+++ Assets/Script/Framework/Assets/NewAssetTest/Editor/AssetbundlePackerEditor.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Script.Framework.Assets.NewAssetTest.Editor
+{
+    class AssetbundlePackerEditor:EditorWindow
+    {
+        private string m_strUGUIAtlasPath = Application.dataPath + "/ResourcePacker/UGUIAtlas/";
+        private string m_strNGUIAtlasPath = Application.dataPath + "/ResourcePacker/NGUIAtlas/";
+        private string m_strDependentAssetRootPath = Application.dataPath + "/ResourcePacker/Asset/";
+        private string m_strOutputPath = Application.dataPath + "/../output/";
+        private AssetbundlePacker m_Handler;
+
+        [MenuItem("Editors/ResourcePacker/ResourcePacker Editor")]
+        public static void Open()
+        {
+            CreateInstance<AssetbundlePackerEditor>().Show();
+        }
+
+        public AssetbundlePackerEditor()
+        {
+            m_Handler = new AssetbundlePacker();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+            {
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.TextArea("UGUI Pack path");
+                m_strUGUIAtlasPath = EditorGUILayout.TextField(m_strUGUIAtlasPath);
+                EditorGUILayout.EndHorizontal();
+                GUILayout.Space(20f);
+
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.TextArea("NGUI Pack path");
+                m_strNGUIAtlasPath = EditorGUILayout.TextField(m_strNGUIAtlasPath);
+                EditorGUILayout.EndHorizontal();
+                GUILayout.Space(20f);
+
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.TextArea("Asset Pack path");
+                m_strDependentAssetRootPath = EditorGUILayout.TextField(m_strDependentAssetRootPath);
+                EditorGUILayout.EndHorizontal();
+                GUILayout.Space(20f);
+
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.TextArea("Output path");
+                m_strOutputPath = EditorGUILayout.TextField(m_strOutputPath);
+                EditorGUILayout.EndHorizontal();
+                
+                GUILayout.Space(20f);
+                EditorGUILayout.BeginHorizontal();
+                {
+                    if (GUILayout.Button("Auto Set bundle Name", GUILayout.Width(300f)))
+                    {
+                        AutosetBundle();
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+
+                GUILayout.Space(20f);
+                EditorGUILayout.BeginHorizontal();
+                {
+                    if (GUILayout.Button("Reset bundle Name", GUILayout.Width(300f)))
+                    {
+                        ResetBundleName();
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+
+                GUILayout.Space(20f);
+                EditorGUILayout.BeginHorizontal();
+                {
+                    if (GUILayout.Button("Begin build Bundle", GUILayout.Width(300f)))
+                    {
+                        BeginBuildBundle();
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+
+                GUILayout.Space(20f);
+                EditorGUILayout.BeginHorizontal();
+                {
+                    if (GUILayout.Button("Gen Asset To bundle Find Map", GUILayout.Width(300f)))
+                    {
+                        GenAssetToBundleFindMap();
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndVertical();
+        }
+
+        private void GenAssetToBundleFindMap()
+        {
+            m_Handler.GenAssetToBundleFindMap();
+        }
+
+        private void BeginBuildBundle()
+        {
+            m_Handler.BeginBuild();
+        }
+
+        private void ResetBundleName()
+        {
+            m_Handler.ResetAllBundleName(Application.dataPath);
+        }
+
+        private void AutosetBundle()
+        {
+            m_Handler.BeginSetBundleName(m_strUGUIAtlasPath,m_strNGUIAtlasPath,m_strDependentAssetRootPath,m_strOutputPath,TextureImporterType.Advanced,TextureImporterFormat.ARGB16);
+        }
+    }
+}
